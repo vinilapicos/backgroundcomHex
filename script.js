b273ca0dd2ebe543dda7c3a7e215f2582dc74d4f
Vinicius Corrêa Carvalho <163591794+vinilapicos@users.noreply.github.com>
2024-06-15 14:07:01 -0300

    Upload arquivos do projeto

---
+++ script.js
@@ -0,0 +1,16 @@
+function mudaCor(){
+    var hex_numbers = ['0','1','2','3','4','5','6',
+    '7','8','9','A','B','C','D','E','F'];
+
+    var hexcode = '';
+
+    for(var i=0;i < 6; i++){
+        var random_index = Math.floor(Math.random() 
+        * hex_numbers.length);
+
+        hexcode += hex_numbers[random_index]
+
+    }
+    document.getElementById('codigoHex').innerHTML = hexcode;
+    document.getElementsByTagName('body')[0].style.background = '#' + hexcode;
+}
